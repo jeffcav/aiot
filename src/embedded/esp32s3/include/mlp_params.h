1a6e6e6d0225d147e4b444375bb4460bf8704de9
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2023-01-12 09:05:26 -0300

    WIP pure fixed point implementation

--- src/embedded/esp32s3/include/mlp_params.h
+++ src/embedded/esp32s3/include/mlp_params.h
@@ -4,21 +4,28 @@
 #include <stdint.h>
 
 extern const int8_t input[165];
-extern const float input_zero;
+extern const int8_t output;
+extern const int8_t input_zero;
 extern const float input_scale;
 
+#define layer_1_fpmult 0.7873240920947224
+extern const int32_t layer_1_multiplier;
+extern const int32_t layer_1_rshifts;
 extern const int8_t layer_1_weights[15840];
-extern const float layer_1_weights_zero;
+extern const int8_t layer_1_weights_zero;
 extern const float layer_1_weights_scale;
 
-extern const float layer_1_zero;
+extern const int8_t layer_1_zero;
 extern const float layer_1_scale;
 
+#define layer_3_fpmult 0.9818835089457887
+extern const int32_t layer_3_multiplier;
+extern const int32_t layer_3_rshifts;
 extern const int8_t layer_3_weights[1440];
-extern const float layer_3_weights_zero;
+extern const int8_t layer_3_weights_zero;
 extern const float layer_3_weights_scale;
 
-extern const float layer_3_zero;
+extern const int8_t layer_3_zero;
 extern const float layer_3_scale;
 
 
--- src/embedded/esp32s3/src/main.cpp
+++ src/embedded/esp32s3/src/main.cpp
@@ -5,28 +5,36 @@
 
 #include "esp_dsp.h"
 
+#define L1_FXP_MULT ((int32_t)(layer_1_fpmult * (1<<31)))
+#define L3_FXP_MULT ((int32_t)(layer_3_fpmult * (1<<31)))
 
 // input read in runtime
 char buffer[165];
 char y;
 
 // floating point layers outputs
-float layer1[96];
-float layer3[15];
+// float layer1[96];
+// float layer3[15];
+
+int8_t layer1[96];
+int8_t layer3[15];
 
 // quantized layers outputs
-int8_t qlayer1[96];
-int8_t qlayer3[15];
+// int8_t qlayer1[96];
+// int8_t qlayer3[15];
 
 // final estimated subject
 int subject_id;
 
 struct qparams {
-    float zero, scale;
+    int8_t zero;
+    float scale;
 };
 
 struct qlayer {
     struct qparams input, weights, output;
+    int32_t multiplier;
+    int32_t rshifts;
 };
 
 struct qlayer l1_qparams, l3_qparams;
@@ -59,7 +67,32 @@ void mvm(struct qlayer *qlayer, const int8_t *M, const int8_t* x, float *out, in
     }
 }
 
+void mvm2(struct qlayer *qlayer, const int8_t *M, const int8_t* x, int8_t *out, int rows, int cols) {
+    int i, j;
+
+    for (i = 0; i < rows; i++) {
+        int8_t res;
+        int16_t mult;
+        int32_t acc = 0;
+
+        for (j = 0; j < cols; j++) {
+            mult = (M[i*cols + j] - qlayer->weights.zero) * (x[j] - qlayer->input.zero);
+            acc += mult;
+        }
+
+        acc = ((acc * qlayer->multiplier) >> qlayer->rshifts) + (int32_t)qlayer->output.zero;
+        res = (int8_t) (acc);
+        out[i] = res;
+    }
+}
+
 void relu(float *x, float *out, int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        out[i] = x[i]>0?x[i]:0;
+}
+
+void qrelu(int8_t *x, int8_t *out, int size) {
     int i;
     for (i = 0; i < size; i++)
         out[i] = x[i]>0?x[i]:0;
@@ -90,6 +123,23 @@ int argmax(float *x, int size) {
     return i_max;
 }
 
+int qargmax(int8_t *x, int size) {
+    int8_t max;
+    int i, i_max;
+
+    max = x[0];
+    i_max = 0;
+
+    for (i=1; i<size; i++) {
+        if (x[i] > max) {
+            max = x[i];
+            i_max = i;
+        }
+    }
+
+    return i_max;
+}
+
 void setup() {
     l1_qparams = {
         .input = {
@@ -103,7 +153,9 @@ void setup() {
         .output = {
             .zero = layer_1_zero,
             .scale = layer_1_scale,
-        }
+        },
+        .multiplier = L1_FXP_MULT,
+        .rshifts = layer_1_rshifts
     };
 
     l3_qparams = {
@@ -118,7 +170,9 @@ void setup() {
         .output = {
             .zero = layer_3_zero,
             .scale = layer_3_scale,
-        }
+        },
+        .multiplier = L3_FXP_MULT,
+        .rshifts = layer_3_rshifts
     };
 
     Serial.begin(921600);
@@ -132,12 +186,17 @@ void loop() {
 
         unsigned int start_b = dsp_get_cpu_cycle_count();
 
-        mvm(&l1_qparams, layer_1_weights, (int8_t*)buffer, layer1, 96, 165);
-        relu(layer1, layer1, 96);
-        quantize(layer1, &l1_qparams.output, 96, qlayer1);
-        mvm(&l3_qparams, layer_3_weights, qlayer1, layer3, 15, 96);
+        // mvm(&l1_qparams, layer_1_weights, (int8_t*)buffer, layer1, 96, 165);
+        // relu(layer1, layer1, 96);
+        // quantize(layer1, &l1_qparams.output, 96, qlayer1);
+        // mvm(&l3_qparams, layer_3_weights, qlayer1, layer3, 15, 96);
 
-        subject_id = argmax(layer3, 15);
+        mvm2(&l1_qparams, layer_1_weights, (int8_t*)buffer, layer1, 96, 165);
+        qrelu(layer1, layer1, 96);
+
+        mvm2(&l3_qparams, layer_3_weights, layer1, layer3, 15, 96);
+        subject_id = qargmax(layer3, 15);
+
         unsigned int end_b = dsp_get_cpu_cycle_count();
 
         Serial.println(subject_id);
